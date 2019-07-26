7ec7d2a659f0d89af50ca0731b0db733e4d6d687
occludedarp <cmrnmr@gmail.com>
2019-07-26 16:29:18 -0400

    added some lines in readme

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -138,9 +138,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
 /* Request 1: .forEach()
-
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
-
 */
 const animalNames = [];
 
@@ -152,12 +150,10 @@ zooAnimals.forEach(nameAndSciName);
 
 console.log(animalNames);
 
+
 /* Request 2: .map()    
-
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-
 */
-
 const lowerCase = [];
 
 function makeLower(animal){
@@ -168,16 +164,23 @@ zooAnimals.map(makeLower)
 
 console.log(lowerCase); 
 
+
 /* Request 3: .filter() 
-
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
-
+The zoos are concerned about animals with a lower population count. Find out which animals have a population less than 5.
 */
 const lowerPopulation = [];
+
+function returnPopulation(animal){
+
+  lowerPopulation.push
+
+}
+
+
 console.log(lowerPopulation);
 
-/* Request 4: .reduce() 
 
+/* Request 4: .reduce() 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
